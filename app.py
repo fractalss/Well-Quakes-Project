c015e48af4f7357ca86262ec9d61db017583dfc6
Olutomiwaxl <oloyedeolutomiwa@yahoo.com>
2019-06-11 13:20:30 -0500

    data jsonify

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ import os
 
 #print(os.environ)
 
-if not os.environ['DYNO']:
+if not os.environ.get('DYNO'):
     import config
     print(config.name)
 
-if os.environ["JAWSDB_URL"]:
+if os.environ.get("JAWSDB_URL"):
     dburl = os.environ["JAWSDB_URL"]
 else:
-    dburl = "sqlite://somesqlite"
+    dburl = config.dburl
 
 engine = sqlalchemy.create_engine(dburl)
 df = pandas.read_sql("SELECT * FROM wells_data", engine)
-
+print(df)
 #print(config.name)
 
 app = Flask(__name__)
@@ -29,7 +29,7 @@ def home():
 
 @app.route("/data")
 def data():
-    return jsonify({"data":"is empty"})
+    return jsonify(df.to_json(orient = "records"))
 
 if __name__ == "__main__":
     app.run()
