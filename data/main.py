631c80257a433fca41a3c8c9e1bfe33c78cc43b6
Olutomiwaxl <oloyedeolutomiwa@yahoo.com>
2019-06-13 20:25:36 -0500

    cleaning

--- data/main.py
+++ data/main.py
@@ -2,13 +2,13 @@ import sqlite3
 import pandas as pd
 
 # load data
-df = pd.read_csv('loving_WellData.csv')
+df = pd.read_csv('loving.csv')
 
 # strip whitespace from headers
 df.columns = df.columns.str.strip()
-del df['Production_type']
-df.dropna
-con = sqlite3.connect("lovingWells.sqlite")
+
+df.dropna()
+con = sqlite3.connect("wells.sqlite")
 
 # drop data into database
 df.to_sql("well_data", con)
