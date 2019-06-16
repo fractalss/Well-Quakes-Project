1bc0909701333a5927c3aef4ab75e567a1b3aad3
Saurav Mitra <saurav.mitra@gmail.com>
2019-06-15 20:56:11 -0500

    making more changes to geo map, changing container in index.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,14 @@
 // Creating map object
 var map = L.map("map", {
-    center: [31.91, -103.67],
-    zoom: 10
+    center: [31.81, -103.67],
+    zoom: 11
   });
   
   // Adding tile layer
   L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox.streets",
+    id: "mapbox.streets-satellite",
     accessToken: API_KEY
   }).addTo(map);
   
@@ -27,22 +27,34 @@ var map = L.map("map", {
     var long = data[i].Long;
     var operator = data[i].Operator;
     var apiWellNumber = data[i].API;
+    var spudDate = data[i].Spud_Date;
+    var prodType = data[i].Production_type;
     var oilProduction = data[i].Daily_Gas;
     var gasProduction = data[i].Daily_Oil;
     var dailyProduction = oilProduction + gasProduction/6;
-    console.log(data[i].Daily_Gas);
-    // Check for location property
+    // Color the well depending upon whether it is Oil or Gas
+
+    let color = "";
+    if (prodType === "OIL") {
+      color = "blue";
+    }
+    else if (prodType === "GAS"){
+      color = "red";
+    }
+    // Check for data
     if (dailyProduction) {
-    color = "red";
     L.circle([lat,long], {
-      fillOpacity: 0.75,
+      fillOpacity: 0.5,
       color: "white",
       fillColor: color,
       // Adjust radius
       radius: dailyProduction*0.5
     
-  }).bindPopup("<h5>" +"API Well Number : "+apiWellNumber+"</h5><hr><p>"+"Operator : " + operator +
-  "</p><hr><p> Daily Oil Production : " + oilProduction + "(BBLS) </p><hr><p> Daily Gas Production : " + gasProduction + "(BBLS)").addTo(map);
+  }).bindPopup("<h3>" +"API Well Number : "+apiWellNumber+"</h3><hr><p>"+"Operator : " 
+  + operator +"</p><hr><p> Daily Oil Production : " + oilProduction + 
+  "(BBLS) </p><hr><p> Daily Gas Production : "+ 
+  gasProduction + "(BBLS) </p><hr><p> Spud Date : " + spudDate).addTo(map);
+  console.log(prodType);
 }
   }
 
